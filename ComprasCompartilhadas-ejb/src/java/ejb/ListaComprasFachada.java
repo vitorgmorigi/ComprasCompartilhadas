cfae4059498b56fde86a654c898bb869034b9d11
igorglatz <38442938+igorglatz@users.noreply.github.com>
2019-05-12 13:50:00 -0300

    implements ListaComprasFachada and ProdutoFachada
    implementação baseada na implementação de Usuario e UsuarioFachada

--- ComprasCompartilhadas-ejb/src/java/ejb/ListaComprasFachada.java
+++ ComprasCompartilhadas-ejb/src/java/ejb/ListaComprasFachada.java
@@ -5,8 +5,13 @@
  */
 package ejb;
 
+import java.math.BigDecimal;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -15,7 +20,31 @@ import javax.ejb.LocalBean;
 @Stateless
 @LocalBean
 public class ListaComprasFachada {
+ @PersistenceContext(unitName = "ComprasCompartilhadas-ejbPU")
+    private EntityManager em;
 
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
+        // Metodo que retorna a lista de compras armazenada na tabela ListaCompras
+    public List<ejb.ListaCompras> getListasCompras() {
+        Query query = em.createNamedQuery("ListaCompras.findAll");
+        return query.getResultList();
+    }
+    
+public int getMaxId() {  // pega o maior ID de lista de compras na tabela
+    
+   Query query = em.createNativeQuery("SELECT MAX(id) FROM LISTA_COMPRAS");
+   BigDecimal a = (BigDecimal) query.getSingleResult();
+   int b = a.intValue();
+   return b;
+}
+ 
+public void cadastrarListaCompras(ListaCompras listaCompras) {  // Cadastra o produto 
+   listaCompras.setId(getMaxId()+1);
+   em.persist(listaCompras);
+}
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 }
